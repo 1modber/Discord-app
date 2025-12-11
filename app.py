312eb00fbbe66848dd8eb4b686e9ded9fd70477d
1modber <140179431+1modber@users.noreply.github.com>
2025-12-11 10:59:56 +0100

    Implement Flask app for Discord interactions
    dfghhg

---
+++ app.py
@@ -0,0 +1,74 @@
+import os
+import json
+from flask import Flask, request, jsonify
+from nacl.signing import VerifyKey
+from nacl.exceptions import BadSignatureError
+import requests
+import time
+
+app = Flask(__name__)
+
+# ---------------------------------------
+PUBLIC_KEY = "A389FD3350D03EF99F9B98F711061B8F1E1E54B6472C746CC0551AE426A131EC"
+APPLICATION_ID = "1448609543356547143"
+# ---------------------------------------
+
+
+def verify_signature(req):
+    signature = req.headers.get("X-Signature-Ed25519")
+    timestamp = req.headers.get("X-Signature-Timestamp")
+    body = req.data.decode("utf-8")
+
+    if not signature or not timestamp:
+        return False
+
+    try:
+        key = VerifyKey(bytes.fromhex(PUBLIC_KEY))
+        key.verify(
+            (timestamp + body).encode(),
+            bytes.fromhex(signature)
+        )
+        return True
+    except BadSignatureError:
+        return False
+
+
+@app.route("/interactions", methods=["POST"])
+def interactions():
+    if not verify_signature(request):
+        return "invalid signature", 401
+
+    data = request.json
+
+    # Discord ping
+    if data["type"] == 1:
+        return jsonify({"type": 1})
+
+    # Slash command
+    if data["type"] == 2:
+        command = data["data"]["name"]
+
+        if command == "sayapp":
+            send_followups(data)
+            return jsonify({"type": 5})   # deferred message
+    return jsonify({"type": 4, "data": {"content": "Unknown command"}})
+
+
+def send_followups(interaction):
+    token = interaction["token"]
+
+    url = f"https://discord.com/api/v10/webhooks/{APPLICATION_ID}/{token}"
+
+    message = "NEBULA ON TOP\nINV: https://discord.gg/rQsZh77Trw"
+
+    for i in range(3):
+        payload = {
+            "content": message,
+            "allowed_mentions": {"parse": []}
+        }
+        requests.post(url, json=payload)
+        time.sleep(0.4)
+
+
+if __name__ == "__main__":
+    app.run(port=3000)
